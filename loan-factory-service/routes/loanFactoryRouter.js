e39dab4db0b150b2e419b5be2841f96e4459f0a0
Thomas Boltze <thomas.boltze@external.kreditech.com>
2018-06-21 16:40:07 +0200

    Added stub of loan factory service

---
+++ loan-factory-service/routes/loanFactoryRouter.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const Joi = require('joi');
+const router = express.Router();
+
+const loanApplicationSchema = Joi.object({
+  amount: Joi.number().integer().positive().max(100000).required(),
+  maturity: Joi.number().integer().positive().min(3).max(60).required(),
+  email: Joi.string().email().required()
+});
+
+/* POST /loan-factory */
+/*  calls user service to create  / get user
+    scoring service to get aproval
+    installment plan calculator to create plan
+    loans service to store the loans
+*/
+router.post('/', function(req, res, next) {
+  const validation = Joi.validate(req.body, loanApplicationSchema, {
+    allowUnknown: false,
+    abortEarly: true
+  }).then(validation => {
+    res.status(201);
+    res.json({amount: req.body.amount});
+    res.end();
+  }).catch(error => {
+    res.status(400);
+    res.json(error.details[0]);
+    res.end()
+  })
+});
+
+
+module.exports = router;
---
+++ loan-factory-service/test/api/loanFactoryTest.js
@@ -0,0 +1,35 @@
+// During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+process.env.PORT = '3000';
+
+// Require the dev-dependencies
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../app');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+// Our parent block
+describe('Loan Factory', () => {
+  /*
+  * Happy path for scores
+  */
+  describe('POST /loan-factory', () => {
+    it('it should create and user when amount =< 10000', (done) => {
+      chai.request(server)
+        .post('/loan-factory')
+        .send({amount: 5000, maturity: 12, email: 'mike@example.com'})
+        .end((err, res) => {
+          console.log(res.body)
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          res.body.amount.should.be.eq(5000);
+          //res.body.status.should.be.eq('approved');
+          //res.body.interest_rate.should.be.eq(6);
+          done();
+        });
+    });
+
+  });
+});
