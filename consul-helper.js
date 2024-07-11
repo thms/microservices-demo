b27316a2aaac2985a9b1d59a9367dc8437e8a0e4
Thomas Boltze <thomas.boltze@gmail.com>
2024-07-11 12:33:26 +0100

    Update consul-helper.js

--- consul-helper.js
+++ consul-helper.js
@@ -5,16 +5,16 @@ const listServices = () => consul.agent.service.list(function(err, result) {
   console.log(result)
 });
 
-{ 'loan-service-12345':
-   { ID: 'loan-service-12345',
-     Service: 'loan-service',
-     Tags: [ 'node', 'rest' ],
-     Address: '10.94.1.37',
-     Meta: {},
-     Port: 3000,
-     EnableTagOverride: false,
-     CreateIndex: 0,
-     ModifyIndex: 0 } }
+// { 'loan-service-12345':
+//    { ID: 'loan-service-12345',
+//      Service: 'loan-service',
+//      Tags: [ 'node', 'rest' ],
+//      Address: '10.94.1.37',
+//      Meta: {},
+//      Port: 3000,
+//      EnableTagOverride: false,
+//      CreateIndex: 0,
+//      ModifyIndex: 0 } }
 
 const getServiceNodes = (serviceName) => consul.catalog.service.nodes(serviceName, function(err, result) {
   if (err) throw err;
